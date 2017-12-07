acd66e6ea816bbedce5d8782ca60f230f71bc9a4
ShubhraJain <00sankalp@gmail.com>
2017-12-07 13:22:45 -0800

    Solved n Queens problem

--- src/solvers.js
+++ src/solvers.js
@@ -116,8 +116,28 @@ window.findNQueensSolution = function(n) {
 
 // return the number of nxn chessboards that exist, with n queens placed such that none of them can attack each other
 window.countNQueensSolutions = function(n) {
-  var solutionCount = undefined; //fixme
+    var solutionCount = 0; //fixme
+    var board = new Board({'n': n});
 
-  console.log('Number of solutions for ' + n + ' queens:', solutionCount);
-  return solutionCount;
+    var findSolution = function(board, row, n) {
+        if (row === n) {
+            solutionCount++;
+            return;
+        }   
+
+        for (var col = 0; col < n; col++) {
+            board.togglePiece(row, col);
+            if (!board.hasAnyQueensConflicts()) {
+                var result = findSolution(board, row + 1, n);
+                if (result) {
+                    return result;
+                }
+            }
+            board.togglePiece(row, col);
+        }
+    }
+    findSolution(board, 0, n)
+
+    console.log('Number of solutions for ' + n + ' queens:', solutionCount);
+    return solutionCount;
 };
